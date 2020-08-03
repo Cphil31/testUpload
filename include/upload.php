ae532aa9cce03ca6d923898804f02039a4ad7cf9
Cphil31 <cecilephilippe31@gmail.com>
2020-08-03 18:05:22 +0200

    dernier version

--- include/upload.php
+++ include/upload.php
@@ -5,27 +5,31 @@ if(!empty($_FILES)){
    
     
     $file_type = $_FILES ['fichier']['type'];
-
-
+    
+    
+    
     //destination du fichier qu'on envoie
     $file_tmp_name = $_FILES ['fichier']['tmp_name'];
-
+    
     // taille du fichier en octet 
     $file_size= $_FILES ['fichier']['size'];
     
-
     // afficher l'extention du fichier 
     $file_extention = strchr($file_name,".");
-  
+    
+    
 
     //repertoire de destination
     $file_dest = 'files/'.$file_name;
-
+    
     // on crée un tableau pour l'authaurisation de l'extention 
-    $extention_valid = array('.pdf','.PDF');
-
+    $extention_valid = array('.pdf','.PDF','.jpg','.jpeg','.jfif');
+    
     // on vérifie si l'extention fait partie du tableau authorisé 
     if(in_array($file_extention,$extention_valid)){
+        var_dump("Temp name = ".$file_tmp_name);
+        var_dump($file_extention);
+
         // on vas vérifier le fichier de destination du fichier qu'on envoie . 
         // on vas l'envoyer dans le dossier files .
         if(move_uploaded_file($file_tmp_name,$file_dest)){
@@ -34,6 +38,9 @@ if(!empty($_FILES)){
             $req = $db->prepare($q);
             $req->execute(array($file_name,$file_dest));
             echo 'fichier envoyé avec succes';
+        }
+        else{
+            echo "le fichier n'as pas été envoyé";
         }
         
     }
