586da442c23a520391befdc1658838795a85358d
Cphil31 <cecilephilippe31@gmail.com>
2020-08-03 10:28:59 +0200

    affichage taille fichier

--- index.php
+++ index.php
@@ -5,12 +5,27 @@ var_dump($_FILES);
         var_dump($file_name);
         $file_type = $_FILES ['fichier']['type'];
         var_dump($file_type);
+
+        // taille du fichier en octet 
+        $file_size= $_FILES ['fichier']['size'];
+        var_dump($file_size);
+
         // afficher l'extention du fichier 
         $file_extention = strchr($file_name,".");
         var_dump($file_extention);
 
         // on crée un tableau pour l'authaurisation de l'extention 
-        
+        $extention_valid = array('.pdf','.PDF');
+
+        // on vérifie si l'extention fait partie du tableau authorisé 
+        if(in_array($file_extention,$extention_valid)){
+            // on vas l'envoyer a la detination voulue 
+            
+        }
+        else{
+            echo 'seuls les fichier pdf sont autorisées';
+        }
+
     }
 ?>
 <!DOCTYPE html>
@@ -24,6 +39,7 @@ var_dump($_FILES);
     <h1>Uploader un fichier pdf </h1>
     <p>nom : <?= $file_name ?></p>
     <p>type : <?= $file_type ?></p>
+    <p>taille : <?= $file_size ?> octets</p>
 
     <form action="index.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="fichier"> <br>
