09297b0af9012637dca998a251aad99ee55448be
Cphil31 <cecilephilippe31@gmail.com>
2020-08-03 11:36:21 +0200

    ajout fichier pdf fini

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php 
-var_dump($_FILES);
-    if(!empty($_FILES)){
-        $file_name = $_FILES ['fichier']['name'];
-        var_dump($file_name);
-        $file_type = $_FILES ['fichier']['type'];
-        var_dump($file_type);
-
-        // taille du fichier en octet 
-        $file_size= $_FILES ['fichier']['size'];
-        var_dump($file_size);
-
-        // afficher l'extention du fichier 
-        $file_extention = strchr($file_name,".");
-        var_dump($file_extention);
-
-        // on crée un tableau pour l'authaurisation de l'extention 
-        $extention_valid = array('.pdf','.PDF');
-
-        // on vérifie si l'extention fait partie du tableau authorisé 
-        if(in_array($file_extention,$extention_valid)){
-            // on vas l'envoyer a la detination voulue 
-            
-        }
-        else{
-            echo 'seuls les fichier pdf sont autorisées';
-        }
-
-    }
+require_once 'include/upload.php';
+require_once 'include/connexion.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,13 +11,19 @@ var_dump($_FILES);
 </head>
 <body>
     <h1>Uploader un fichier pdf </h1>
-    <p>nom : <?= $file_name ?></p>
-    <p>type : <?= $file_type ?></p>
-    <p>taille : <?= $file_size ?> octets</p>
 
     <form action="index.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="fichier"> <br>
         <input type="submit" value="envoyer le fichier">
     </form>
+
+    <h1>Fichiers enregistrés </h1>
+    <?php 
+    $q = "SELECT * FROM files";
+    $req = $db->prepare($q);
+    $req->execute();
+    var_dump($req->fetchAll());
+
+    ?>
 </body>
 </html>
